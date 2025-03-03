9049dba76b8ceea6c33c81245c43a93cf32ce08f
AvaAvarai <alice.williams.tech@gmail.com>
2025-03-02 18:19:00 -0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PrototypeCourseAndLessonPageUI/',
+  base: 'https://cs481educationalplatform.github.io/PrototypeCourseAndLessonPageUI/',
 })
